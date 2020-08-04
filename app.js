3c1335d098384db7c3540a8757a7b0464a7034d8
Cameron Graham <cameron.g882@gmail.com>
2020-08-04 18:54:22 -0400

    created the switch case function to allow different questions to basked for different emp types

--- app.js
+++ app.js
@@ -4,20 +4,85 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+newMember();
+function newMember() {
+
+    inquirer
+        .prompt({
+            type: "list",
+            name: "title",
+            message: "What is the Job Title of this employee?",
+            choices: [
+                "Manager",
+                "Engineer",
+                "Intern",
+                "No more employees in team"
+            ]
+        })
+        .then(({ title }) => {
+            // switch and case for every type of employee
+            switch (title) {
+                case "Manager":
+                    managerCreate();
+                    break;
+
+                case "Engineer":
+                    engineerCreate();
+                    break;
+
+                case "Intern":
+                    internCreate();
+                    break;
+
+                case "No more employees in team":
+                    generateHTML();
+                    break;
+
+            }
+        })
+}
+
+// Manager inquire function
+function managerCreate(){
+    inquirer
+    .prompt([
+        {
+        
+        }
+    ])
+}
+
+function engineerCreate(){
+    inquirer
+    .prompt([
+        {
+        
+        }
+    ])
+}
+
+function internCreate(){
+    inquirer
+    .prompt([
+        {
+        
+        }
+    ])
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+function generateHTML(){
 
+}
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
